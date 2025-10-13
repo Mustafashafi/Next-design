01ddb9f92cf193ba2aebaff3fde1a009d14e0d44
Mustafashafi <mustafashafi143@gmail.com>
2025-10-13 13:11:53 +0500

    Updated homepage

--- components/Brands.js
+++ components/Brands.js
@@ -1,11 +1,38 @@
+import Image from "next/image";
+
 export default function Brands() {
   return (
     <section id="home" className="brands">
-        <img src="/images/1.png" alt="" />
-        <img src="/images/2.png" alt="" />
-        <img src="/images/3.png" alt="" />
-        <img src="/images/4.png" alt="" />
-      
+      <div className="brands-container">
+        <Image
+          src="/images/1.png"
+          alt="Brand 1"
+          width={200}
+          height={120}
+          className="brand-image"
+        />
+        <Image
+          src="/images/2.png"
+          alt="Brand 2"
+          width={200}
+          height={120}
+          className="brand-image"
+        />
+        <Image
+          src="/images/3.png"
+          alt="Brand 3"
+          width={200}
+          height={120}
+          className="brand-image"
+        />
+        <Image
+          src="/images/4.png"
+          alt="Brand 4"
+          width={200}
+          height={120}
+          className="brand-image"
+        />
+      </div>
     </section>
   );
 }
