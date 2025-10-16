c949437d8c35bcc6ac86519b0a677f42d298f897
Mustafashafi <mustafashafi143@gmail.com>
2025-10-16 14:55:51 +0500

    Updated homepage

--- components/Contact.js
+++ components/Contact.js
@@ -6,7 +6,7 @@ export default function Contact() {
       <div className="contact-wrapper">
         {/* LEFT SIDE - TEXT CONTENT */}
         <div className="contact-text" style={{ marginTop: "-400px" }}>
-          <h2 className="contact-title">Let’s Work Together</h2>
+          <h2 className="contact-title" style={{color:"#186cb5"}}>Let’s Work Together</h2>
           <p className="contact-subtitle">
             Have a project in mind? Let’s discuss how <span>AI can transform</span> your business.
             Fill out the form and our team will reach out shortly.
